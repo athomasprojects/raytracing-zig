0762812c9aeafa222bf7276ffa530c0e08d57557
athomasprojects <amanda.thomas14@gmail.com>
2025-04-16 03:31:52 -0400

    add vec3 data structure

--- in-one-weekend/src/main.zig
+++ in-one-weekend/src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 const ppm = @import("ppm.zig");
+const vec3 = @import("vec3.zig");
+const Vec3 = vec3.Vec3;
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
 
 const ppm_fname = "image02.ppm";
 
@@ -13,4 +17,112 @@ pub fn main() !void {
     const image_height: usize = 5 * 256;
     const path = ppm.IMG_DIR ++ ppm_fname;
     try ppm.createFile(image_width, image_height, path, allocator);
+}
+
+test "vec3.create" {
+    const v = Vec3.create(1, 2, 3);
+    try expect(std.meta.eql(v, @Vector(3, f64){ 1, 2, 3 }));
+}
+
+test "vec3.createEmpty" {
+    const v = Vec3.createEmpty();
+    const expected: @Vector(3, f64) = [_]f64{0} ** 3;
+    try expect(std.meta.eql(v, expected));
+}
+
+test "vec3.fromScalar" {
+    const c: f64 = 5.018972;
+    const v = Vec3.fromScalar(c);
+    const expected: @Vector(3, f64) = [_]f64{c} ** 3;
+    try expect(std.meta.eql(v, expected));
+}
+
+test "vec3.loadArr" {
+    const arr: [3]f64 = .{ 2, 5, -6.9 };
+    const v = Vec3.loadArr(arr);
+    try expect(std.meta.eql(v, arr));
+}
+
+test "vec3.xyz" {
+    const v = Vec3.create(1, 2, 3);
+    const x = Vec3.x(v);
+    const y = Vec3.y(v);
+    const z = Vec3.z(v);
+    try expect(x == 1);
+    try expect(y == 2);
+    try expect(z == 3);
+}
+
+test "vec3.divByScalar" {
+    var v = Vec3.create(1, 2, 3);
+    v = Vec3.divByScalar(v, 2);
+    try expect(std.meta.eql(v, [_]f64{ 0.5, 1, 1.5 }));
+}
+
+test "vec3.dot" {
+    const u = Vec3.create(1, 0, 0);
+    const v = Vec3.create(0, 1, 0);
+
+    const a = Vec3.create(1, -1, 1);
+    const b = Vec3.create(0.5, 1, 1);
+    try expect(Vec3.dot(u, v) == 0);
+    try expect(Vec3.dot(a, b) == 0.5);
+}
+
+test "vec3.cross" {
+    const x = Vec3.create(1, 0, 0);
+    const y = Vec3.create(0, 1, 0);
+    const z = Vec3.create(0, 0, 1);
+    const zero = [_]f64{0} ** 3;
+    try expectEqual(Vec3.cross(x, y), z);
+    try expectEqual(Vec3.cross(y, z), x);
+    try expectEqual(Vec3.cross(z, x), y);
+    try expectEqual(Vec3.cross(x, z), -y);
+    try expectEqual(Vec3.cross(x, x), zero);
+    try expectEqual(Vec3.cross(-x, x), zero);
+}
+
+test "vec3.lengthSquared" {
+    const v = Vec3.create(1, 1, 1);
+    const u = Vec3.create(1, 1, 0);
+    const v_lsq = Vec3.lengthSquared(v);
+    const u_lsq = Vec3.lengthSquared(u);
+    try expect(v_lsq == 3);
+    try expect(u_lsq == 2);
+}
+
+test "vec3.length" {
+    const u = Vec3.create(-1, 0, 0);
+    const v = Vec3.create(1, 0, 1);
+    const w = Vec3.create(1, 1, 1);
+    try expect(Vec3.length(u) == 1);
+    try expect(Vec3.length(v) == vec3.sqrt2);
+    try expect(Vec3.length(w) == vec3.sqrt3);
+}
+
+test "vec3.unitVector" {
+    const v = Vec3.create(1, -1, -1);
+    const v_norm = Vec3.unitVector(v);
+    const norm_vlen = Vec3.length(v_norm);
+    try expect(norm_vlen == 1);
+}
+
+test "vec3.containsTwo" {
+    const u = Vec3.create(1, -1, 0);
+    const v = Vec3.create(1, 1, 0);
+    const w = Vec3.create(-1, -1, 1);
+
+    try expect(Vec3.containsTwo(@abs(u), 1));
+    try expect(Vec3.containsTwo(v, 1));
+    try expect(!Vec3.containsTwo(@abs(w), 1));
+}
+
+test "vec3.containsThree" {
+    const u = Vec3.create(1, -1, 1);
+    const v = Vec3.create(1, 1, -1);
+    const w = Vec3.create(-1, -1, -1);
+
+    try expect(Vec3.containsThree(@abs(u), 1));
+    try expect(Vec3.containsThree(@abs(v), 1));
+    try expect(Vec3.containsThree(@abs(w), 1));
 }
---
+++ in-one-weekend/src/vec3.zig
@@ -0,0 +1,114 @@
+const std = @import("std");
+const math = std.math;
+const sqrt = math.sqrt;
+
+pub const Vec3Error = error{
+    TooManyElements,
+};
+
+pub const F64x3 = @Vector(3, f64);
+pub const sqrt2: f64 = sqrt(@as(f64, math.sqrt2));
+pub const sqrt3: f64 = sqrt(@as(f64, 3));
+
+pub const unit_vec_x = F64x3{ 1, 0, 0 };
+pub const unit_vec_y = F64x3{ 0, 1, 0 };
+pub const unit_vec_z = F64x3{ 0, 0, 1 };
+
+pub const Point3 = F64x3;
+
+pub const Vec3 = struct {
+    pub fn createEmpty() F64x3 {
+        return @splat(0);
+    }
+
+    pub fn create(e0: f64, e1: f64, e2: f64) F64x3 {
+        return .{ e0, e1, e2 };
+    }
+
+    pub fn fromScalar(value: f64) F64x3 {
+        return @splat(value);
+    }
+
+    pub fn loadArr(arr: [3]f64) F64x3 {
+        return .{ arr[0], arr[1], arr[2] };
+    }
+
+    pub fn x(v: F64x3) f64 {
+        return v[0];
+    }
+
+    pub fn y(v: F64x3) f64 {
+        return v[1];
+    }
+
+    pub fn z(v: F64x3) f64 {
+        return v[2];
+    }
+
+    pub fn divByScalar(v: F64x3, t: f64) F64x3 {
+        return v / @as(F64x3, @splat(t));
+    }
+
+    pub fn length(v: F64x3) f64 {
+        const abs_v: F64x3 = @abs(v);
+        if (isEql(abs_v, unit_vec_x) or isEql(abs_v, unit_vec_y) or isEql(abs_v, unit_vec_z)) {
+            return 1;
+        } else if (containsTwo(abs_v, 1.0)) {
+            return sqrt2;
+        } else if (containsThree(abs_v, 1.0)) {
+            return sqrt3;
+        }
+
+        return sqrt(lengthSquared(v));
+    }
+
+    pub fn lengthSquared(v: F64x3) f64 {
+        return @reduce(std.builtin.ReduceOp.Add, v * v);
+    }
+
+    pub fn dot(u: F64x3, v: F64x3) f64 {
+        return @reduce(std.builtin.ReduceOp.Add, u * v);
+    }
+
+    pub fn cross(u: F64x3, v: F64x3) F64x3 {
+        // Shuffle index sets for cross product.
+        // x = u.y*v.z - u.z*v.y
+        // y = u.z*v.x - u.x*v.z
+        // z = u.x*v.y - u.y*v.x
+
+        const yzx_mask = [_]i32{ 1, 2, 0 };
+        const zxy_mask = [_]i32{ 2, 0, 1 };
+        const u_yzx = @shuffle(f64, u, u, yzx_mask);
+        const v_zxy = @shuffle(f64, v, v, zxy_mask);
+        const u_zxy = @shuffle(f64, u, u, zxy_mask);
+        const v_yzx = @shuffle(f64, v, v, yzx_mask);
+
+        return u_yzx * v_zxy - u_zxy * v_yzx;
+    }
+
+    pub fn unitVector(v: F64x3) F64x3 {
+        return divByScalar(v, length(v));
+    }
+
+    fn containsN(v: F64x3, value: f64, num: u8) bool {
+        const comparison: @Vector(3, bool) = v == fromScalar(value);
+        const count: u8 = @reduce(std.builtin.ReduceOp.Add, @as(@Vector(3, u8), @intFromBool(comparison)));
+        return count == num;
+    }
+
+    pub fn containsTwo(v: F64x3, value: f64) bool {
+        return containsN(v, value, 2);
+    }
+
+    pub fn containsThree(v: F64x3, value: f64) bool {
+        return containsN(v, value, 3);
+    }
+
+    pub fn isEql(u: F64x3, v: F64x3) bool {
+        return @reduce(std.builtin.ReduceOp.And, u == v);
+    }
+
+    pub fn print(v: F64x3) void {
+        std.debug.print("{{{d}, {d}, {d}}}\n", .{ v[0], v[1], v[2] });
+    }
+};
