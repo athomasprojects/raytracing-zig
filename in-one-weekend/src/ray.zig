70d2592400a023712a744e5f2a97d1f10244c657
athomasprojects <amanda.thomas14@gmail.com>
2025-04-23 16:28:22 -0400

    fix ray.zig imports from vec.zig

--- in-one-weekend/src/ray.zig
+++ in-one-weekend/src/ray.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const Colour = @import("vec.zig").Colour;
 const Point3 = @import("vec.zig").Point3;
 const F64x3 = @import("vec.zig").F64x3;
+const Vec3 = @import("vec.zig").Vec3;
 
 pub const Ray = struct {
     orig: Point3,
@@ -20,6 +21,6 @@ pub const Ray = struct {
     }
 
     pub fn at(self: Ray, t: f64) Point3 {
-        return self.orig + F64x3.scale(self, t);
+        return self.orig + Vec3.scale(self, t);
     }
 };
