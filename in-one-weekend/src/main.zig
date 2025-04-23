276820e222ea457ca0101fdc32845b233a2c5ac7
athomasprojects <amanda.thomas14@gmail.com>
2025-04-23 16:26:37 -0400

    move Vec3 tests to vec.zig, move ppm file rendering to main.zig

--- in-one-weekend/src/main.zig
+++ in-one-weekend/src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
-const ppm = @import("ppm.zig");
 const vec = @import("vec.zig");
+const colour = @import("colour.zig");
 const Ray = @import("ray.zig").Ray;
 const Vec3 = vec.Vec3;
 const Point3 = vec.Point3;
@@ -8,133 +8,55 @@ const Colour = vec.Colour;
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
-const ppm_fname = "image02.ppm";
-
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
     const allocator = arena.allocator();
 
-    const image_width: usize = 5 * 256;
-    const image_height: usize = 5 * 256;
-    const path = ppm.IMG_DIR ++ ppm_fname;
-    try ppm.createFile(image_width, image_height, path, allocator);
-}
+    const ppm_fname = "image03.ppm";
+    const ppm_dir = "images/ppm/";
+    const path = ppm_dir ++ ppm_fname;
 
-test "vec.create" {
-    const v = Vec3.create(1, 2, 3);
-    try expect(std.meta.eql(v, @Vector(3, f64){ 1, 2, 3 }));
-}
+    const aspect_ratio = 16.0 / 9.0;
+    const image_width = 1200;
+    const image_height: comptime_int = @intFromFloat(@as(comptime_float, image_width) / aspect_ratio);
 
-test "vec.createEmpty" {
-    const v = Vec3.createEmpty();
-    const expected: @Vector(3, f64) = [_]f64{0} ** 3;
-    try expect(std.meta.eql(v, expected));
-}
+    const magic_number = "P3";
+    const max_colour = 255;
 
-test "vec.fromScalar" {
-    const c: f64 = 5.018972;
-    const v = Vec3.fromScalar(c);
-    const expected: @Vector(3, f64) = [_]f64{c} ** 3;
-    try expect(std.meta.eql(v, expected));
-}
+    // Create buffered writer to stdout.
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
 
-test "vec.loadArr" {
-    const arr: [3]f64 = .{ 2, 5, -6.9 };
-    const v = Vec3.loadArr(arr);
-    try expect(std.meta.eql(v, arr));
-}
+    // Create or open ppm file.
+    const file = try std.fs.cwd().createFile(path, std.fs.File.CreateFlags{ .read = true });
+    defer file.close();
 
-test "vec.xyz" {
-    const v = Vec3.create(1, 2, 3);
-    const x = Vec3.x(v);
-    const y = Vec3.y(v);
-    const z = Vec3.z(v);
-    try expect(x == 1);
-    try expect(y == 2);
-    try expect(z == 3);
-}
+    const file_writer = file.writer();
+    var buffer = std.ArrayList(u8).init(allocator);
+    const buf_writer = buffer.writer();
 
-test "vec.scale" {
-    var v = Vec3.create(1, 2, -3.5);
-    v = Vec3.scale(v, 2);
-    try expect(std.meta.eql(v, Vec3.create(2, 4, -7)));
-}
+    // Render
+    try buf_writer.print("{s}\n{d} {d}\n{d}\n", .{ magic_number, image_width, image_height, max_colour });
 
-test "vec.divScalar" {
-    var v = Vec3.create(1, 2, 3);
-    v = Vec3.divScalar(v, 2);
-    try expect(std.meta.eql(v, [_]f64{ 0.5, 1, 1.5 }));
-}
+    var pixel_colour: Colour = Vec3.createEmpty();
+    for (0..image_height) |j| {
+        // Progress indicator.
+        try stdout.print("Scanlines remaining: {d}\r", .{image_height - j});
+        try bw.flush();
 
-test "vec.dot" {
-    const u = Vec3.create(1, 0, 0);
-    const v = Vec3.create(0, 1, 0);
-
-    const a = Vec3.create(1, -1, 1);
-    const b = Vec3.create(0.5, 1, 1);
-    try expect(Vec3.dot(u, v) == 0);
-    try expect(Vec3.dot(a, b) == 0.5);
-}
-
-test "vec.cross" {
-    const x = Vec3.create(1, 0, 0);
-    const y = Vec3.create(0, 1, 0);
-    const z = Vec3.create(0, 0, 1);
-    const zero = Vec3.createEmpty();
-    try expectEqual(Vec3.cross(x, y), z);
-    try expectEqual(Vec3.cross(y, z), x);
-    try expectEqual(Vec3.cross(z, x), y);
-    try expectEqual(Vec3.cross(x, z), -y);
-    try expectEqual(Vec3.cross(x, x), zero);
-    try expectEqual(Vec3.cross(-x, x), zero);
-}
-
-test "vec.lengthSquared" {
-    const u = Vec3.create(1, 1, 0);
-    const v = Vec3.create(1, 1, 1);
-    const w = Vec3.create(-1, 1, 0);
-    const u_l2 = Vec3.lengthSquared(u);
-    const v_l2 = Vec3.lengthSquared(v);
-    const w_l2 = Vec3.lengthSquared(w);
-    try expect(u_l2 == 2);
-    try expect(v_l2 == 3);
-    try expect(w_l2 == 2);
-}
-
-test "vec.length" {
-    const u = Vec3.create(-1, 0, 0);
-    const v = Vec3.create(1, 0, 1);
-    const w = Vec3.create(1, 1, 1);
-    try expect(Vec3.length(u) == 1);
-    try expect(Vec3.length(v) == vec.sqrt2);
-    try expect(Vec3.length(w) == vec.sqrt3);
-}
-
-test "vec.normalize" {
-    const v = Vec3.create(1, -1, -1);
-    const v_norm = Vec3.normalize(v);
-    const norm_vlen = Vec3.length(v_norm);
-    try expect(norm_vlen == 1);
-}
-
-test "vec.hasTwoOnes" {
-    const u = Vec3.create(1, -1, 0);
-    const v = Vec3.create(1, 1, 0);
-    const w = Vec3.create(-1, -1, 1);
-
-    try expect(Vec3.hasTwoOnes(u));
-    try expect(Vec3.hasTwoOnes(v));
-    try expect(!Vec3.hasTwoOnes(w));
-}
-
-test "vec.isAllOnes" {
-    const u = Vec3.create(1, -1, 1);
-    const v = Vec3.create(1, 1, -1);
-    const w = Vec3.create(-1, -1, -1);
-
-    try expect(Vec3.isAllOnes(u));
-    try expect(Vec3.isAllOnes(v));
-    try expect(Vec3.isAllOnes(w));
+        for (0..image_width) |i| {
+            pixel_colour = Vec3.create(
+                @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(image_width - 1)),
+                @as(f64, @floatFromInt(j)) / @as(f64, @floatFromInt(image_height - 1)),
+                @as(f64, 0.0),
+            );
+            try colour.writeColour(buf_writer, pixel_colour);
+        }
+    }
+    try file_writer.writeAll(buffer.items);
+    try stdout.print("\rDone.                     \n", .{});
+    try bw.flush();
 }
--- in-one-weekend/src/vec.zig
+++ in-one-weekend/src/vec.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const math = std.math;
 const sqrt = math.sqrt;
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
 
 pub const F64x3 = @Vector(3, f64);
 pub const Point3 = F64x3;
@@ -115,3 +117,120 @@ pub const Vec3 = struct {
         std.debug.print("{{{d}, {d}, {d}}}\n", .{ v[0], v[1], v[2] });
     }
 };
+
+test "vec.create" {
+    const v = Vec3.create(1, 2, 3);
+    try expect(std.meta.eql(v, @Vector(3, f64){ 1, 2, 3 }));
+}
+
+test "vec.createEmpty" {
+    const v = Vec3.createEmpty();
+    const expected: @Vector(3, f64) = [_]f64{0} ** 3;
+    try expect(std.meta.eql(v, expected));
+}
+
+test "vec.fromScalar" {
+    const c: f64 = 5.018972;
+    const v = Vec3.fromScalar(c);
+    const expected: @Vector(3, f64) = [_]f64{c} ** 3;
+    try expect(std.meta.eql(v, expected));
+}
+
+test "vec.loadArr" {
+    const arr: [3]f64 = .{ 2, 5, -6.9 };
+    const v = Vec3.loadArr(arr);
+    try expect(std.meta.eql(v, arr));
+}
+
+test "vec.xyz" {
+    const v = Vec3.create(1, 2, 3);
+    const x = Vec3.x(v);
+    const y = Vec3.y(v);
+    const z = Vec3.z(v);
+    try expect(x == 1);
+    try expect(y == 2);
+    try expect(z == 3);
+}
+
+test "vec.scale" {
+    var v = Vec3.create(1, 2, -3.5);
+    v = Vec3.scale(v, 2);
+    try expect(std.meta.eql(v, Vec3.create(2, 4, -7)));
+}
+
+test "vec.divScalar" {
+    var v = Vec3.create(1, 2, 3);
+    v = Vec3.divScalar(v, 2);
+    try expect(std.meta.eql(v, [_]f64{ 0.5, 1, 1.5 }));
+}
+
+test "vec.dot" {
+    const u = Vec3.create(1, 0, 0);
+    const v = Vec3.create(0, 1, 0);
+
+    const a = Vec3.create(1, -1, 1);
+    const b = Vec3.create(0.5, 1, 1);
+    try expect(Vec3.dot(u, v) == 0);
+    try expect(Vec3.dot(a, b) == 0.5);
+}
+
+test "vec.cross" {
+    const x = Vec3.create(1, 0, 0);
+    const y = Vec3.create(0, 1, 0);
+    const z = Vec3.create(0, 0, 1);
+    const zero = Vec3.createEmpty();
+    try expectEqual(Vec3.cross(x, y), z);
+    try expectEqual(Vec3.cross(y, z), x);
+    try expectEqual(Vec3.cross(z, x), y);
+    try expectEqual(Vec3.cross(x, z), -y);
+    try expectEqual(Vec3.cross(x, x), zero);
+    try expectEqual(Vec3.cross(-x, x), zero);
+}
+
+test "vec.lengthSquared" {
+    const u = Vec3.create(1, 1, 0);
+    const v = Vec3.create(1, 1, 1);
+    const w = Vec3.create(-1, 1, 0);
+    const u_l2 = Vec3.lengthSquared(u);
+    const v_l2 = Vec3.lengthSquared(v);
+    const w_l2 = Vec3.lengthSquared(w);
+    try expect(u_l2 == 2);
+    try expect(v_l2 == 3);
+    try expect(w_l2 == 2);
+}
+
+test "vec.length" {
+    const u = Vec3.create(-1, 0, 0);
+    const v = Vec3.create(1, 0, 1);
+    const w = Vec3.create(1, 1, 1);
+    try expect(Vec3.length(u) == 1);
+    try expect(Vec3.length(v) == sqrt2);
+    try expect(Vec3.length(w) == sqrt3);
+}
+
+test "vec.normalize" {
+    const v = Vec3.create(1, -1, -1);
+    const v_norm = Vec3.normalize(v);
+    const norm_vlen = Vec3.length(v_norm);
+    try expect(norm_vlen == 1);
+}
+
+test "vec.hasTwoOnes" {
+    const u = Vec3.create(1, -1, 0);
+    const v = Vec3.create(1, 1, 0);
+    const w = Vec3.create(-1, -1, 1);
+
+    try expect(Vec3.hasTwoOnes(u));
+    try expect(Vec3.hasTwoOnes(v));
+    try expect(!Vec3.hasTwoOnes(w));
+}
+
+test "vec.isAllOnes" {
+    const u = Vec3.create(1, -1, 1);
+    const v = Vec3.create(1, 1, -1);
+    const w = Vec3.create(-1, -1, -1);
+
+    try expect(Vec3.isAllOnes(u));
+    try expect(Vec3.isAllOnes(v));
+    try expect(Vec3.isAllOnes(w));
+}
